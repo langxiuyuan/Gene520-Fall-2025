b65fb039cce5fa37966bbffdb766348a0e8dcc76
Xiuyuan Lang <107935547+langxiuyuan@users.noreply.github.com>
2025-10-24 15:07:56 -0400

    Update file paths in ChIP_SE.sh script

--- ChIP-seq/ChIP_SE.sh
+++ ChIP-seq/ChIP_SE.sh
@@ -2,15 +2,16 @@
 genome=$1
 file=$2
 
+# set the lib and chromosome size file
 lib=./lib
-
 chrom_size=$lib/$genome.chrom.sizes
+
 #--------map raw fastq file to reference genome----------------
 
 perl $lib/bowtie_fastq.pl mm10 8 $file # fastq --> sam
 
 #-----------Sam file to sorted mapped bam file----------------
-ref=/mnt/rds/genetics01/JinLab/xww/Reference_Index/$genome/$genome.fa.fai
+ref=/mnt/vstor/courses/gene520/ChIP-seq/Ref/mm10.fa.fai/$genome.fa.fai
 
 
 samtools view -bS -t $ref $file | samtools sort -o ${file/.sam}.bam #sam->sorted bam
@@ -27,7 +28,7 @@ wait
 echo -ne $name has `wc -l $name.monoclonal.bed` monoclonal reads >>summary.reads_count
 $lib/extend_reads_from_start.pl $name.monoclonal.bed 300 $genome | $lib/UCSC_2_normal.pl | sort -k1,1 -k2,2n -k3,3n | $lib/sorted_bed_2_wig_no_strand.pl | $lib/normal_2_UCSC.pl > $name.wig.300bp
 wait
-/home/xww/file_code/ChIP-seq/wigToBigWig $name.wig.300bp $chrom_size $name.bw #convert wig file to bigwig file
+/mnt/vstor/courses/gene520/ChIP-seq/software/wigToBigWig $name.wig.300bp $chrom_size $name.bw #convert wig file to bigwig file
 
 macs3 callpeak -t $name.sorted.bam -n $name
 
